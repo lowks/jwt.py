d4648668442bc1bdbb81173c7fa7b6dbb67f07db
Steve Gregory <contact@steve-gregory.com>
2013-09-05 12:15:32 -0700

    Added setup.py for distributed installation

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(
+    name='jwt.py',
+    version='0.1',
+    author='David Knapp',
+    author_email='dknapp@iplantcollaborative.org',
+    py_modules=['jwt'],
+)
