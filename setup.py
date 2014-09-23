541b4449333041edd9465d4bbf361ab315887431
J. Matt Peterson <jmatt@jmatt.org>
2014-09-23 15:10:38 -0700

    Changes for initial pypi release. 	new file:   AUTHORS 	new file:   CHANGES 	modified:   LICENSE 	new file:   MANIFEST.in 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from distutils.core import setup
+
+
 setup(
     name='jwt.py',
-    version='0.1',
-    author='David Knapp',
-    author_email='dknapp@iplantcollaborative.org',
+    version='0.1.0',
+    author='iPlant Collaborative',
+    author_email='atmodevs@gmail.com',
     py_modules=['jwt'],
     install_requires=[
-        'pycrypto >= 2.6.1, < 2.7'
+        'pycrypto >= 2.6'
     ]
 )
