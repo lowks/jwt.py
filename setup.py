61dfe5c2f6e16aa1525207c06a2c2c62bf6ecda4
Low Kian Seong <kianseong@gmail.com>
2014-09-24 11:23:32 +0800

    Update setup.py
    Add more metadata to `setup.py` to be picked up by `pypi` during package release

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ setup(
     version='0.1.0',
     author='iPlant Collaborative',
     author_email='atmodevs@gmail.com',
+    description='jwt.py',
+    url='https://github.com/iPlantCollaborativeOpenSource/jwt.py',
     py_modules=['jwt'],
     install_requires=[
         'pycrypto >= 2.6'
